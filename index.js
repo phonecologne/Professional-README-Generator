8b1a2b99b6aff29f9ee16e968d5023f902f8dcda
Greg <aquamirror@gmail.com>
2021-11-22 03:20:40 -0800

    Updated more questions
    really hoping this is moving in the right direction

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const generateMarkdown = require('./utils/generateMarkdown');
 // TODO: Create an array of questions for user input
 const questions = [
     {
+        //Naming your Project. Includes the first question and the return question if the user trys to skips this question. I want to include text that lets the user know why answering this question is important
         type: 'input',
         name: 'title',
         //I wanted to ask What is your project's name, BUT the second apostropehe was breaking the code
@@ -15,23 +16,45 @@ const questions = [
             if (nameInput) {
                 return true;
             } else {
-                console.log('Your project must be named! We can not provide without one!');
+                console.log('Your project must be named! We can not proceed without one!');
+                return false;
             }
         }
     },
     {
+        //Linking your GitHub profile to your generated README. 
         type: 'input',
         name: 'github',
-        message: '(Requried!) Enter your GitHub Profile Name',
-        
-    }
+        message: '(Requried!) Enter your GitHub Profile Name.',
+        validate: githubInput => {
+            if (githubInput) {
+                return true;
+            } else {
+                console.log('We need your GitHub Profile Name! We can not proceed without linking your GitHub!')
+                return false;
+            }
+        }
+    },
+    {
+        //After Linking your GitHub profile we are now going to link to the user repository for the project. 
+        type: 'input',
+        name: 'repo',
+        message: '(Required!) Please enter the name of your GitHub Repository.'
+        validate: repoInput => {
+            if (repoInput) {
+                return true;
+            } else {
+                console.log('We need the name of your GitHub repository! Without one we can not create a README for you!')
+            }
+        }
+    },
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) { }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() { }
 
 // Function call to initialize app
 init();
